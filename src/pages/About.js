158385511f7be5d8889cca3559ae9bd561fadc3a
JoelAllen98 <joelallen1198@gmail.com>
2023-01-25 13:13:49 +0000

    Placeholder about info

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export class About extends Lightning.Component {
                 h: 1080,
                 src: Utils.asset('images/background3.png'),
               },
-              Title: {
+            Title: {
                 mount: 0,
                 x: 50,
                 y: 50,
@@ -18,15 +18,50 @@ export class About extends Lightning.Component {
                   fontSize: 64,
                   textColor: 0xbbffffff,
                 },
+            },
+            MovieImage: {
+                rect: true,
+                w: 300,
+                h: 450,
+                x: 200,
+                y: 200,
+                color: 0xffffffff,
               },
-            Info: {
-                x: 960,
-                y: 450,
-                mount: 0.5,
+            MovieTitle: {
+                x: 600,
+                y: 200,
                 text: {
-                    text: 'Here is some info about the page',
-                    fontSize: 32
+                    text: 'Generic Action Movie 7',
+                    fontSize: 64,
                 }
+            },
+            ReleaseDate: {
+                x: 600,
+                y: 300,
+                w: 100,
+                h: 50,
+                text: {
+                    text: "11/06/2017",
+                    fontSize: 18,
+                },
+            },
+
+            Description: {
+                x: 600,
+                y: 350,
+                w: 600,
+                h: 450,
+                text: {
+                    text: "Big, strong, bald man drives cars above the speed limit and causes dozens of casualties with lots of explosions. The seventh instalment of the franchise goes bigger, faster and more explosiony than ever before. The plot is as thin as the plot of the previous six films, but the action is bigger and better. The film is a must-see for fans of the franchise, but it's not for everyone. Why not try watching a film with a plot instead?",
+                    fontSize: 24,
+                },
+                flex: {
+                    direction: 'column',
+                    wrap: true,
+                    justifyContent: 'space-evenly',
+                    alignContent: 'center',
+                    padding: 20,
+                  },
             },
         };
     }
